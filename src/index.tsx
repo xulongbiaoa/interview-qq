e47478ec2f77c70096d59bc47b5b58653e2f183a
xulongbiao <1151051851@qq.com>
2022-05-11 16:39:56 +0800

    remove

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import "./i18n.js"
 import './icons';
 const root = ReactDOM.createRoot(
@@ -20,7 +19,3 @@ root.render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
